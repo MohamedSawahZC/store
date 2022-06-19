98bbccb47ce47eb8ac0c5b1ef8491edddb8b71b6
unknown <sawah810@gmail.com>
2022-06-19 07:50:13 +0200

    Handle server middlewares

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const helmet_1 = __importDefault(require("helmet"));
+const morgan_1 = __importDefault(require("morgan"));
+const express_rate_limit_1 = require("express-rate-limit");
 const server = (0, express_1.default)();
+server.use((0, morgan_1.default)('common')); // Logger middleware to show any request happent in server
+server.use((0, helmet_1.default)()); //For HTTP Security
+server.use(express_1.default.json()); //For parsing json bodies
+//================= handle rate limit for security from spam bots =====================
+server.use((0, express_rate_limit_1.rateLimit)({
+    windowMs: 60 * 1000,
+    max: 2,
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: 'Too many request from this IP, please try again after an hour', // Disable the `X-RateLimit-*` headers
+}));
 server.get('/', (req, res) => {
     res.send({
         message: 'Welcome dude',
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
 import express, { Application, Request, Response } from 'express';
-
+import helmet from 'helmet';
+import morgan from 'morgan';
+import RateLimit, { rateLimit } from 'express-rate-limit';
 const server: Application = express();
+
+server.use(morgan('common')); // Logger middleware to show any request happent in server
+
+server.use(helmet()); //For HTTP Security
+
+server.use(express.json()); //For parsing json bodies
+
+//================= handle rate limit for security from spam bots =====================
+
+server.use(
+  rateLimit({
+    windowMs: 60 * 1000, // 15 minutes
+    max: 2, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false,
+    message: 'Too many request from this IP, please try again after an hour', // Disable the `X-RateLimit-*` headers
+  })
+);
 
 server.get('/', (req: Request, res: Response) => {
   res.send({
