70a03251732ff6d8bea08a08d0cd5a5059cb84cd
unknown <sawah810@gmail.com>
2022-06-19 08:04:35 +0200

    Handling errors

--- dist/index.js
+++ dist/index.js
@@ -7,18 +7,27 @@ const express_1 = __importDefault(require("express"));
 const helmet_1 = __importDefault(require("helmet"));
 const morgan_1 = __importDefault(require("morgan"));
 const express_rate_limit_1 = require("express-rate-limit");
+const error_middleware_1 = __importDefault(require("./middleware/error.middleware"));
 const server = (0, express_1.default)();
 server.use((0, morgan_1.default)('common')); // Logger middleware to show any request happent in server
 server.use((0, helmet_1.default)()); //For HTTP Security
 server.use(express_1.default.json()); //For parsing json bodies
+server.use(error_middleware_1.default);
 //================= handle rate limit for security from spam bots =====================
 server.use((0, express_rate_limit_1.rateLimit)({
     windowMs: 60 * 1000,
-    max: 2,
+    max: 100,
     standardHeaders: true,
     legacyHeaders: false,
     message: 'Too many request from this IP, please try again after an hour', // Disable the `X-RateLimit-*` headers
 }));
+//============ Handle route not found =============
+server.use((_req, res) => {
+    res.status(404).json({
+        message: 'ooh you are lost maybe page not found',
+    });
+});
+//=================================================
 server.get('/', (req, res) => {
     res.send({
         message: 'Welcome dude',
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express, { Application, Request, Response } from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import RateLimit, { rateLimit } from 'express-rate-limit';
+import errorMiddleware from './middleware/error.middleware';
+
 const server: Application = express();
 
 server.use(morgan('common')); // Logger middleware to show any request happent in server
@@ -10,18 +12,27 @@ server.use(helmet()); //For HTTP Security
 
 server.use(express.json()); //For parsing json bodies
 
+server.use(errorMiddleware);
 //================= handle rate limit for security from spam bots =====================
 
 server.use(
   rateLimit({
     windowMs: 60 * 1000, // 15 minutes
-    max: 2, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false,
     message: 'Too many request from this IP, please try again after an hour', // Disable the `X-RateLimit-*` headers
   })
 );
 
+//============ Handle route not found =============
+server.use((_req: Request, res: Response) => {
+  res.status(404).json({
+    message: 'ooh you are lost maybe page not found',
+  });
+});
+
+//=================================================
 server.get('/', (req: Request, res: Response) => {
   res.send({
     message: 'Welcome dude',
